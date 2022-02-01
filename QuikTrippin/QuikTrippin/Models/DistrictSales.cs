27ed084299b537082dff8e0a4e8b665b712528ec
Dario Perez <darioperez1415@gmail.com>
2022-01-31 20:45:33 -0600

    Added comments

--- QuikTrippin/QuikTrippin/Models/DistrictSales.cs
+++ QuikTrippin/QuikTrippin/Models/DistrictSales.cs
@@ -8,5 +8,9 @@ namespace QuikTrippin.Models
 {
     internal class DistrictSales
     {
+        //Enter the district sales
+        //Store sales
+        //Enter store sales should have store manager sales, Assistant Manager sales, and Associate Sales 
+        //
     }
 }
