219a6f21e0c6c72d64a060ab14266431ffef1883
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-01-31 19:10:42 -0600

    creating district and store classes

---
+++ QuikTrippin/QuikTrippin/Models/District.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikTrippin.Models
+{
+    internal class District
+    {
+        public string Name { get; set; }
+        public string DistrictManager { get; set; }
+
+    }
+}
---
+++ QuikTrippin/QuikTrippin/Models/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikTrippin.Models
+{
+    internal class Store
+    {
+        public Store()
+        {
+
+        }
+
+        public int StoreNumber { get; set; }
+    }
+}
